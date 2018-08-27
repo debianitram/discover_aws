ab5e3fe1273cc0107f4af9ec36699366722a28ef
Martín Miranda <martin.miranda@invgate.com>
2018-08-27 08:38:30 -0300

    agrego método get_pricing

--- discover_aws.py
+++ discover_aws.py
@@ -1,10 +1,13 @@
 import boto3
-from pprint import pprint
+from botocore.exceptions import ClientError
+
+import logging
 
 # Config ~/.aws/config
 # [default]
 # aws_access_key_id = ...
 # aws_secret_access_key = ...
+logger = logging.getLogger(__name__)
 
 
 region = 'us-east-1'
@@ -44,15 +47,35 @@ class DiscoverEC2(object):
         return 'Undefined'
 
     def get_operating_system(self, instance):
-        platform = getattr(instance.image, 'platform', 'Undefined')
+        # The value is Windows for Windows AMIs; otherwise blank.
+        platform = getattr(instance.image, 'platform', 'Linux')
         if not platform:
-            platform = 'Undefined'
+            platform = 'Linux'
 
         return platform
 
     def get_total_storage(self, instance):
         # Expresado en Gb.
         return sum([volume.size for volume in instance.volumes.all()])
+
+    def get_owner_id(self):
+        # https://boto3.readthedocs.io/en/latest/reference/services/sts.html?highlight=sts
+        sts = boto3.client('sts')
+        identity = sts.get_caller_identity()
+        return indentity['Account']
+
+    # Pricing
+    def get_pricing(self):
+        # https://boto3.readthedocs.io/en/latest/reference/services/pricing.html
+        try:
+            pricing = boto3.client('pricing', self.region)
+            describe_services = pricing.describe_services()
+        except ClientError as client_error:
+            logger.warning(client_error.message)
+            describe_services = client_error.message
+
+        return describe_services
+
 
 
 if __name__ == '__main__':
